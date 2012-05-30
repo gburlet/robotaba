20cceb8b9e3349d88858bc8d17127332a1ce715e
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-05-29 20:37:34 -0400

    Setup Django
    Create view for the home page.

---
+++ robotaba/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'robotaba.views.home'),
+    url(r'^admin/', include(admin.site.urls))
+)
