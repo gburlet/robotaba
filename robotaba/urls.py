1a9ccf8a86ce69524989d46cf62d97c5e00c0144
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-08-05 22:24:29 -0400

    Serve media files
    Allow media files to be viewed/listened to via a url to the file in the
    media root.

--- robotaba/urls.py
+++ robotaba/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'robotaba.views.home'),
+    url(r'^$', 'robotaba.views.home', name='home_url'),
+    url(r'^about/', 'robotaba.views.about', name='about_url'),
+    url(r'^contact/', 'robotaba.views.contact', name='contact_url'),
     url(r'^transcribe/upload/$', 'transcribe.views.upload_audio', name='transcribe_upload'),
     url(r'^transcribe/(?P<audio_id>\d+)/$', 'transcribe.views.process'),
     url(r'^pitchestimate/upload/$', 'pitchestimate.views.upload_audio', name='pitchestimate_upload'),
@@ -12,5 +15,6 @@ urlpatterns = patterns('',
     url(r'^tabulate/upload/$', 'tabulate.views.upload_score', name='tabulate_upload'),
     url(r'^tabulate/(?P<pmei_id>\d+)/$', 'tabulate.views.process'),
     url(r'^tabulate/display/(?P<tmei_id>\d+)/$', 'tabulate.views.display'),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^admin/', include(admin.site.urls))
 )
